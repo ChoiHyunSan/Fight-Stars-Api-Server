b62e77e106f2c4a2476179bd96405ba0c1d66c22
ChoiHyunSan <chsan626@gmail.com>
2025-04-17 18:51:40 +0900

    feat: RefreshToken에서 UserID를 정수형으로 관리하도록 수정

---
+++ Domain/Auth/Entities/Dtos/AuthRefresh.cs
@@ -0,0 +1,9 @@
+﻿public class RefreshRequest
+{
+    public string RefreshToken { get; set; } = string.Empty;
+}
+
+public class RefreshResponse
+{
+    public string accessToken { get; set; } = string.Empty;
+}
