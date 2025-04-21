e2eaa65edac749025d428fcd4b14c87b86be460a
ChoiHyunSan <chsan626@gmail.com>
2025-04-21 22:54:45 +0900

    feat: Game Data를 로딩하는 컨트롤러 구현

---
+++ Presentation/Controllers/GameDataController.cs
@@ -0,0 +1,42 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+/***************************
+
+     GameDataController
+
+***************************/
+// Description
+// : 게임 데이터에 관련된 API를 처리하는 컨트롤러입니다.
+//   게임 데이터는 게임의 상태, 플레이어 정보, 게임 설정 등을 포함합니다.
+//
+// Author : ChoiHyunSan
+
+[Route("api/data")]
+[ApiController]
+public class GameDataController : ControllerBase
+{
+    private readonly IUserLoadDataService _userLoadDataService;
+    private readonly IJwtService _jwtService;
+
+    public GameDataController(
+        IUserLoadDataService userLoadDataService,
+        IJwtService jwtService)
+    {
+        _userLoadDataService = userLoadDataService;
+        _jwtService = jwtService;
+    }
+
+    [HttpGet("load")]
+    [Authorize]
+    public async Task<IActionResult> GetUserData()
+    {
+        var userId = _jwtService.GetUserIdFromClaims(User);
+        var userData = await _userLoadDataService.LoadUserDataAsync(userId);
+        return Ok(userData);
+    }
+
+
+}
